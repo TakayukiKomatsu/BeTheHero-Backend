6bcb29ce277f08f70a2bb3f904811cfa9f2a1794
TakayukiKomatsu <takayuki.komatsu@fatec.sp.gov.br>
2020-04-19 18:26:59 -0300

    index profileController

--- src/routes.js
+++ src/routes.js
@@ -6,11 +6,15 @@ const routes = express.Router();
 const ongsController = require("./controllers/ongController");
 const incidentsController = require("./controllers/incidentsController");
 
+const profileController = require("./controllers/profileController");
+
 routes.post("/ongs", ongsController.create);
 routes.get("/ongs", ongsController.index);
 
 routes.get("/incidents", incidentsController.index);
 routes.post("/incidents", incidentsController.create);
 routes.delete("/incidents/:id", incidentsController.delete);
+
+routes.get("/profile", profileController.index);
 
 module.exports = routes;
